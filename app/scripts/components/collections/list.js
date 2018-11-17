cbaf93f00aafba223314db9f8f6fd07a7b4ca961
Matt Savoie <savoie@nsidc.org>
2018-11-17 15:48:39 -0700

    no lint problems

--- app/scripts/components/collections/list.js
+++ app/scripts/components/collections/list.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import moment from 'moment';
 import {
   clearCollectionsSearch,
-  cumulusInstanceMetadata,
+  // cumulusInstanceMetadata,
   listCollections,
   searchCollections
 } from '../../actions';
