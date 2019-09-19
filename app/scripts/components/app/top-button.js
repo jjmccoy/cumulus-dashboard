ffeaae69196f289d23893f69d906de3b8d4fe3a9
Juanisa McCoy <juanisa.j.mccoy@nasa.gov>
2019-09-19 19:22:36 -0400

    add top button controls and other pagination component

---
+++ app/scripts/components/app/top-button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class TopButton extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    topPage() {
+        window.scrollTo(0,0);
+    }
+
+  
+    render () {
+        return (
+            <div className='top'>
+                <button className='cdash-btn--top' onClick={this.topPage}> Go To Top of Page
+                </button>
+            </div>
+        );
+    }
+
+}
+
+export default TopButton;
+
